1774931565dc62f500d73b0519cd0546c79f5282
jeanfredrik <jeanfredrik@gmail.com>
2017-05-25 11:58:42 +0200

    🎉 Initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,22 @@
+import gulp from 'gulp';
+import run from 'run-sequence';
+import babel from 'gulp-babel';
+
+gulp.task('js', () => (
+  gulp.src('src/**/*.js')
+  .pipe(babel())
+  .pipe(gulp.dest('dist'))
+));
+
+gulp.task('watch', () => {
+  gulp.watch('src/**/*.js', ['js']);
+});
+
+gulp.task('build', ['js']);
+
+gulp.task('dev', run(
+  ['build'],
+  ['watch'],
+));
+
+gulp.task('default', ['build']);
